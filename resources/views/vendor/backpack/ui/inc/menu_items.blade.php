f68722653e0b6c8d2468be5fbc9d4bb625a30fa3
yafiazka <yafiazka19@gmail.com>
2026-08-19 10:56:04 +0700

    feat(admin): configure Backpack sidebar menu navigation for articles, categories, tags, comments, and users

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,2 +1,11 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
+
+<li class="nav-separator">MANAJEMEN BERITA</li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('article') }}"><i class="la la-newspaper nav-icon"></i> <span>Artikel Berita</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('category') }}"><i class="la la-list nav-icon"></i> <span>Kategori</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('tag') }}"><i class="la la-tag nav-icon"></i> <span>Tags</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('comment') }}"><i class="la la-comments nav-icon"></i> <span>Komentar</span></a></li>
+
+<li class="nav-separator">PENGATURAN & USER</li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="la la-users nav-icon"></i> <span>Pengguna (Users)</span></a></li>
