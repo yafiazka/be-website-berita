6cb1549e380b5ae9e0f488da869a93dce39ba18f
yafiazka <yafiazka19@gmail.com>
2026-08-29 20:20:23 +0700

    hotfix/update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CategorySeeder::class,
             TagSeeder::class,
-            ArticleSeeder::class,
+            // ArticleSeeder::class,
         ]);
     }
 }
